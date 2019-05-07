850ea2b0a3e62fd01f37037f9af0496c2be26536
Emilio Quiambao <emilioquiambao@gmail.com>
2019-05-06 17:53:15 -0700

    Added delete function. Polished functions.

--- simpleBlogApp/views.py
+++ simpleBlogApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
 from .models import Post
 from .forms import PostForm
@@ -12,41 +13,33 @@ def index(request):
 
 
 def postNew(request):
-    posts = Post.objects.all().order_by('-post_id')
-
     if request.method == 'POST':
         form = PostForm(request.POST)
-
         if form.is_valid():
             submission = form.save(commit=False)
             submission.save()
-            return render(request, 'index.html', context={'posts': posts})
+            return redirect('index')
         else:
             return render(request, 'error.html')
-
-    else:
-        return render(request, 'index.html', context={'posts': posts})
+    return redirect('index')
 
 
 def postDetail(request, id):
     if Post.objects.filter(post_id = id).exists():
         post = Post.objects.get(post_id=id)
         return render(request, 'post.html', context={'post': post})
-    else:
-        return render(request, 'error.html')
+    return render(request, 'error.html')
 
 
 def postDelete(request, id):
     if Post.objects.filter(post_id = id).exists():
-        post = Post.objects.get(post_id=id)
-        return render(request, 'post.html', context={'post': post})
-    else:
-        return render(request, 'error.html')
+        Post.objects.filter(post_id = id).delete()
+        return redirect('index')
+    return render(request, 'error.html')
 
 
 def postEdit(request, id):
     if Post.objects.filter(post_id = id).exists():
         post = Post.objects.get(post_id=id)
         return render(request, 'post.html', context={'post': post})
-    else:
-        return render(request, 'error.html')
+    return render(request, 'error.html')
